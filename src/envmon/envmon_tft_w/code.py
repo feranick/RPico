047654ab0bc86ac1cd491c2f6731f6ebdb9d168e
Nicola Ferralis <feranick@hotmail.com>
2024-01-09 10:40:14 -0500

    Add text while waiting for NWS sync

--- src/envmon/envmon_tft_w/code.py
+++ src/envmon/envmon_tft_w/code.py
@@ -212,9 +212,12 @@ if serial:
 # https://w1.weather.gov/data/obhistory/KBOS.html
 # sea_level_pressure = 1029.3
 
+labels[2].text = "Retrieving NWS data..."
+
 nws = get_nws_data()
 bme280.sea_level_pressure = nws[2]
 elapsed_sec = 0
+labels[2].text = "                      "
 
 ############################
 # Main

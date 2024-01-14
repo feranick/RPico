f428e5d6e96748d7bc7b4c9ecc36b4f2c0d2f55e
Nicola Ferralis <feranick@hotmail.com>
2024-01-14 15:26:22 -0500

    Add title to page app.

--- src/remote/code.py
+++ src/remote/code.py
@@ -1,6 +1,6 @@
 # **********************************************
 # * Garage Opener - Rasperry Pico W
-# * v2024.01.13.4
+# * v2024.01.14.1
 # * By: Nicola Ferralis <feranick@hotmail.com>
 # **********************************************
 
@@ -50,6 +50,7 @@ class Server:
         <!DOCTYPE html>
         <html>
         <head>
+        <title>Pico Garage Opener</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=0" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta names="apple-mobile-web-app-status-bar-style" content="black-translucent" />

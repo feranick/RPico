df6dc89855f5482011d15afe30bcbb3f80ed2136
Nicola Ferralis <feranick@hotmail.com>
2024-02-03 13:40:00 -0500

    compatibility with circuitpython

---
+++ src/traffic_light/code.py
@@ -0,0 +1,24 @@
+import time
+import board
+import digitalio
+
+red_led = digitalio.DigitalInOut(board.GP11)
+red_led.direction = digitalio.Direction.OUTPUT
+amber_led = digitalio.DigitalInOut(board.GP14)
+amber_led.direction = digitalio.Direction.OUTPUT
+green_led = digitalio.DigitalInOut(board.GP13)
+green_led.direction = digitalio.Direction.OUTPUT
+
+while True:
+    red_led.value = True
+    time.sleep(5)
+    amber_led.value = True
+    time.sleep(2)
+    red_led.value = False
+    amber_led.value = False
+    green_led.value = True
+    time.sleep(5)
+    green_led.value = False
+    amber_led.value = True
+    time.sleep(3)
+    amber_led.value = False

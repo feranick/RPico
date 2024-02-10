41da443d2f486a1f4f28ba82f2304f3bf5b41c97
Nicola Ferralis <feranick@hotmail.com>
2024-02-10 14:33:15 -0500

    Updates

--- src/tests/test_transistor_control.py
+++ src/tests/test_transistor_control.py
@@ -2,13 +2,13 @@ import board
 import digitalio
 import time
 
-btn = digitalio.DigitalInOut(board.GP16)
+btn = digitalio.DigitalInOut(board.GP18)
 btn.direction = digitalio.Direction.OUTPUT
 
 while True:
     btn.value = False
     print(btn.value)
-    time.sleep(3)
+    time.sleep(5)
     btn.value = True
     print(btn.value)
-    time.sleep(3)
+    time.sleep(5)

9cf3d9dc2d0ac49557791699271093ac20d90064
Nicola Ferralis <feranick@hotmail.com>
2025-01-24 17:21:37 -0500

    baseline

--- src/envmon/envmon_tft_w/9.x.x/code.py
+++ src/envmon/envmon_tft_w/9.x.x/code.py
@@ -45,8 +45,8 @@ class Conf:
             self.serial = bool(os.getenv("serial"))
         except:
             self.station = "kbos"
-            self.co2eq_base = 0x9b9b
-            self.tvoc_base = 0xfd17
+            self.co2eq_base = 0x2e6
+            self.tvoc_base = 0xfe26
             self.serial = True
 
         self.url = "https://api.weather.gov/stations/"+self.station+"/observations/latest/"

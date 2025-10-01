bee9cdb52533fc862f0989ff7175148413e04e97
Nicola Ferralis <feranick@hotmail.com>
2025-10-01 12:44:33 -0400

    Remove deprecated html tags

--- src/remote/remote/10.x/code.py
+++ src/remote/remote/10.x/code.py
@@ -137,8 +137,7 @@ class Server:
         <head>
         <title>Pico Garage Opener</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=0" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta names="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="mobile-web-app-capable" content="yes" />
         </head>
         <body>
         <style type="text/css">
